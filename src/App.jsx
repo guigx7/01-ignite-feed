013bd94d6add2dfcfa317c29341b1f1af78c564b
Guilherme Tavares <guigo211103@gmail.com>
2023-01-30 11:52:17 -0300

    Configuradas as Keys

--- src/App.jsx
+++ src/App.jsx
@@ -47,6 +47,7 @@ function App() {
           {posts.map(post => {
             return (
               <Post
+              key={post.id}
               author={post.author}
               content={post.content}
               publishedAt={post.publishedAt}
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -56,10 +56,10 @@ export function Post({ author, publishedAt, content }) {
       <div className={styles.content}>
         {content.map((line) => {
           if (line.type === "paragraph") {
-            return <p>{line.content}</p>;
+            return <p key={line.content}>{line.content}</p>;
           } else if (line.type === "link") {
             return (
-              <p>
+              <p key={line.content}>
                 <a href="#">{line.content}</a>
               </p>
             );
@@ -82,7 +82,10 @@ export function Post({ author, publishedAt, content }) {
 
       <div className={styles.commentList}>
         {comments.map((comment) => {
-          return <Comment content={comment} />;
+          return <Comment
+            key={comment}
+            content={comment}
+          />;
         })}
       </div>
     </article>
