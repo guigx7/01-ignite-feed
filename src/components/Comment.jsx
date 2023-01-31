afb15811aafce05f6aec91b5e8050aada7eb78e2
Guilherme Tavares <guigo211103@gmail.com>
2023-01-31 00:30:05 -0300

    Estruturada a lógica do botão de like

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,11 +1,19 @@
 import { ThumbsUp, Trash } from 'phosphor-react'
+import { useEffect, useState } from 'react';
 import { Avatar } from './Avatar'
 import styles from './Comment.module.css'
 
 export function Comment({ content, onDeleteComment }) {
+  const [likeCount, setLikeCount] = useState(0);
   function handleDeleteComent() {
     onDeleteComment(content);
   }
+
+  function handleLikeComment() {
+    setLikeCount(likeCount + 1);
+    console.log(likeCount);
+    setLikeCount(likeCount + 1);
+  } 
 
   return (
     <div className={styles.comment}>
@@ -28,9 +36,9 @@ export function Comment({ content, onDeleteComment }) {
         </div>
 
         <footer>
-          <button>
+          <button onClick={handleLikeComment}>
             <ThumbsUp />
-            Aplaudir <span>20</span>
+            Aplaudir <span>{likeCount}</span>
           </button>
         </footer>
       </div>
